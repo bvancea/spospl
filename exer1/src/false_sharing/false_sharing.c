1a251d62a94f2e30a6c9ad390d64e920bce76b8d
Bogdan Vancea <bogdan.aurel.vancea@gmail.com>
2013-10-08 16:26:52 +0200

    #1: False sharing Refactored code to allow for measurements.

--- exer1/src/false_sharing/false_sharing.c
+++ exer1/src/false_sharing/false_sharing.c
@@ -1,58 +1,71 @@
+#define _GNU_SOURCE
 #include <sys/times.h>
 #include <time.h>
 #include <stdio.h> 
 #include <pthread.h> 
+#include <sched.h>
+#include "false_sharing.h"
 
-
-long long int tmsBegin1,tmsEnd1,tmsBegin2,tmsEnd2,tmsBegin3,tmsEnd3;
+#define ITERATIONS 1000000000
+#define ADDED_VALUE 1
+long long int tmsBegin1, tmsEnd1, tmsBegin2, tmsEnd2, tmsBegin3, tmsEnd3;
 
 int array[100];
-void heavy_loop(void *param);
 
-void heavy_loop(void *param) {
-  int   index = *((int*)param);
-  int   i;
-  for (i = 0; i < 10000; i++) {
-    array[index]+=3;
-  }
-} 
+int main(int argc, char *argv[]) {
+	int first_elem = 0;
+	int bad_elem = 1;
+	int good_elem = 32;
+	double same_line_time = measure_memory_access(first_elem, bad_elem);
+	printf("Time needed for accessing values in the same cache line: %f\n",same_line_time);
+	check_array_value(first_elem, ADDED_VALUE * ITERATIONS);
+	check_array_value(bad_elem, ADDED_VALUE * ITERATIONS);
+	double different_line_time = measure_memory_access(first_elem, good_elem);
+	printf("Time needed for accessing values in different cache line: %f\n",different_line_time);
+	check_array_value(first_elem, ADDED_VALUE * ITERATIONS);
+	check_array_value(good_elem, ADDED_VALUE * ITERATIONS);
+	return 0;
+}
 
-int main(int argc, char *argv[]) { 
-  int       first_elem  = 0;
-  int       bad_elem    = 1;
-  int       good_elem   = 32;
-  long long time1;
-  long long time2;
-  long long time3;
-  pthread_t     thread_1;
-  pthread_t     thread_2;
+void bind_to_cpu(int cpu) {
+	cpu_set_t mask;
+	CPU_ZERO(&mask);
+	CPU_SET(cpu, &mask);
+	pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &mask);
+}
 
-  tmsBegin3 = clock();
-  heavy_loop((void*)&first_elem);
-  heavy_loop((void*)&bad_elem);
-  tmsEnd3 = clock();
+void heavy_loop(void *args) {
+	arguments* a = (arguments *) args;
+	bind_to_cpu(a->cpu);
+	int i;
+	for (i = 0; i < ITERATIONS; i++) {
+		array[a->index] += ADDED_VALUE;
+	}
+}
 
-  tmsBegin1 = clock();
-  pthread_create(&thread_1, NULL,  (void*)heavy_loop, (void*)&first_elem);
-  pthread_create(&thread_2, NULL,  (void*)heavy_loop, (void*)&bad_elem);
-  pthread_join(thread_1, NULL);
-  pthread_join(thread_2, NULL);
-  tmsEnd1 = clock(); 
+double measure_memory_access(int index1, int index2) {
+	pthread_t thread_1, thread_2;
+	arguments args[2];
+	array[index1] = array[index2] = 0;			//Reset values
 
-  tmsBegin2 = clock();
-  pthread_create(&thread_1, NULL,  (void*)heavy_loop, (void*)&first_elem);
-  pthread_create(&thread_2, NULL,  (void*)heavy_loop, (void*)&good_elem);
-  pthread_join(thread_1, NULL);
-  pthread_join(thread_2, NULL);
-  tmsEnd2 = clock();
+	args[0].index = index1;
+	args[0].cpu = 0;
+	args[1].index = index2;
+	args[1].cpu = 1;
+	clock_t start = clock();
+	pthread_create(&thread_1, NULL, (void*) heavy_loop, (void*) &args[0]);
+	pthread_create(&thread_2, NULL, (void*) heavy_loop, (void*) &args[1]);
+	pthread_join(thread_1, NULL);
+	pthread_join(thread_2, NULL);
 
-  printf("%d %d %d\n", array[first_elem],array[bad_elem],array[good_elem]);
-  time1 = (tmsEnd1-tmsBegin1)*1000/CLOCKS_PER_SEC;
-  time2 = (tmsEnd2-tmsBegin2)*1000/CLOCKS_PER_SEC;
-  time3 = (tmsEnd3-tmsBegin3)*1000/CLOCKS_PER_SEC;
-  printf("%lld ms\n", time1);
-  printf("%lld ms\n", time2);
-  printf("%lld ms\n", time3);
+	clock_t end = clock();
+	return (double) (end - start) / CLOCKS_PER_SEC;
+}
 
-  return 0; 
-} 
+void check_array_value(int index, int expected_value) {
+	if (array[index] == expected_value) {
+		printf("Element %d of the array has the CORRECT value %d.\n", index, expected_value);
+	} else {
+		printf("Element %d of the array has value %d when it should have %d.\n", index, array[index], expected_value);
+	}
+}
