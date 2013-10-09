038d73f777cb9943de943f8d0b2ad5ae063b6ba6
Bogdan Vancea <bogdan.aurel.vancea@gmail.com>
2013-10-09 14:21:11 +0200

    #1: Numa allocation Added code and measurements for the numa allocation exercise.

--- exer1/src/numa_allocation/numa_allocation.c
+++ exer1/src/numa_allocation/numa_allocation.c
@@ -4,29 +4,43 @@
 #define _GNU_SOURCE
 #include <numa.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sched.h>
+#include <pthread.h>
+#include <time.h>
+#include <sys/time.h>
+#include <assert.h>
 #include "numa_allocation.h"
 
+#define ARRAY_SIZE 100000000
 
+void read_parameters(int argc, char **argv, int* node, int* cpu) {
+	assert(argc == 3 && "First parameter is the memory node and the second parameter is the CPU used to run ");
+	*node = atoi(argv[1]);
+	*cpu = atoi(argv[2]);
+	fprintf(stdout, "Allocating memory on node: %d\nExecuting program on CPU: %d.", *node, *cpu);
+	if (numa_node_of_cpu(*cpu) == *node) {
+		fprintf(stdout, "Based on the topology, testing local memory access\n");
+	} else {
+		fprintf(stdout, "Based on the topology, testing remote memory access.\n");
+	}
+}
 
 int main(int argc, char **argv) {
-
-	if (numa_available()) {
-		printf("Numa available.\n");
-	} else {
-		printf("Numa not available.\n");
-	}
-
+	int node, cpu;
 	check_topology();
 
-	numa_set_localalloc();
+	read_parameters(argc, argv, &node, &cpu);
 
+	bind_to_cpu(cpu);
+
+	int* array = (int*) numa_alloc_onnode(ARRAY_SIZE * sizeof(int), node);
+	generate_random_array(array, ARRAY_SIZE);
+	clear_cache();
+	memory_read_latency(array, ARRAY_SIZE, ARRAY_SIZE);
 	return 0;
 }
 
-void bindToNode(int node) {
-
-}
 void check_topology(void) {
 	int cpus = numa_num_task_cpus();
 	int nodes = numa_num_task_nodes();
@@ -43,6 +57,7 @@ void check_topology(void) {
 		cpus_from_bitmask(bm);
 		printf("Memory: %d GB\n", node_size_in_GB(i));
 	}
+	printf("##################################\n");
 
 }
 
@@ -70,17 +85,86 @@ int current_numa_node() {
 }
 
 /**
- * Uses the same method described in the
+ * 	Allocate a large array and access it to make sure the contents of the cache are replaced.
  */
-void bind_to_numa_node(int node) {
-	nodemask_t node_mask;
-	nodemask_zero(&node_mask);
-	/*nodemask_set(&node_mask, node);
-	numa_bind(&node_mask);*/
+void clear_cache() {
+	long long int i = 0;
+	int* array = (int*) malloc(ARRAY_SIZE * sizeof(int));
+	for (i = 0; i < ARRAY_SIZE; i++) {
+		array[i] += 1;
+	}
+	free(array);
 }
 
+/**
+ * Binds the current thread to the CPU cpu.
+ */
 void bind_to_cpu(int cpu) {
-
+	cpu_set_t mask;
+	CPU_ZERO(&mask);
+	CPU_SET(cpu, &mask);
+	pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &mask);
 }
 
+void* generate_random_array(int* array, int size) {
+	fprintf(stdout, "Generating array of integers in the range 0 - %d...\n", size);
+	int i = 0;
+	for (i = 0; i < size; i++) {
+		array[i] = i;
+	}
+	shuffle(array, size);
+	return array;
+}
 
+void shuffle(int *array, int n) {
+	fprintf(stdout, "Shuffling array...\n");
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    int usec = tv.tv_usec;
+    srand48(usec);
+    if (n > 1) {
+        int i;
+        for (i = n - 1; i > 0; i--) {
+            int j = (unsigned int) (drand48()*(i+1));
+            swap(&array[i], &array[j]);
+        }
+    }
+}
+
+void swap(int* a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void print_array(int* array, int size) {
+	int i;
+	for (i = 0; i < size; i++) {
+		printf("%d ",array[i] );
+	}
+	printf("\n");
+}
+
+/**
+ * Will fit into an into on modern cpus, since tv_user guaranteed to be less than 1 million.
+ */
+int get_current_time_ns() {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_usec * 1e3;
+}
+
+void memory_read_latency(int* array, int size, int steps) {
+	struct timeval t0,t1;
+	gettimeofday(&t0, 0);
+	int i;
+	int j = 0;
+	long long int sum;
+	for (i = 0; i < steps; i++) {
+		j = array[j]++;
+		sum += j;
+	}
+	gettimeofday(&t1, 0);
+	long long int elapsed = (t1.tv_sec-t0.tv_sec)*1000000 + t1.tv_usec-t0.tv_usec;
+	printf("\nMemory latency: %f ns\n", (elapsed * 1e3) / steps);
+}
--- exer1/src/numa_allocation/numa_allocation.h
+++ exer1/src/numa_allocation/numa_allocation.h
@@ -8,13 +8,22 @@
 #ifndef NUMA_ALLOCATION_H_
 #define NUMA_ALLOCATION_H_
 
+typedef struct numa_node_t {
+	int node;
+	int* cpu_ids;
+} numa_node;
+
 void check_topology(void);
+void memory_read_latency(int* array, int size, int steps);
+void clear_cache();
+void* generate_random_array(int* array, int size);
+void swap(int* a, int *b);
+void shuffle(int *array, int n);
+void print_array(int* array, int size);
 
 int current_numa_node();
 int node_size_in_GB(int node);
 int* cpus_from_bitmask(struct bitmask *bm) ;
-
 void bind_to_cpu(int cpu);
-void bind_to_numa_node(int node);
 
 #endif /* NUMA_ALLOCATION_H_ */
